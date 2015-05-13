30744d91927814bd3c8bed44961015423dca5adc
John <vidadelaempresa@gmail.com>
2015-05-13 13:53:00 +0100

    Add QuadernoRecurring class

--- quaderno_load.php
+++ quaderno_load.php
@@ -24,6 +24,7 @@ require_once $quaderno_script_path.'quaderno_expense.php';
 require_once $quaderno_script_path.'quaderno_estimate.php';
 require_once $quaderno_script_path.'quaderno_invoice.php';
 require_once $quaderno_script_path.'quaderno_credit.php';
+require_once $quaderno_script_path.'quaderno_recurring.php';
 require_once $quaderno_script_path.'quaderno_document_item.php';
 require_once $quaderno_script_path.'quaderno_payment.php';
 require_once $quaderno_script_path.'quaderno_tax.php';
---
+++ quaderno_recurring.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Quaderno Invoice
+*
+* @package   Quaderno PHP
+* @author    Quaderno <hello@quaderno.io>
+* @copyright Copyright (c) 2015, Quaderno
+* @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+*/
+
+class QuadernoRecurring extends QuadernoDocument
+{
+  static protected $model = 'recurring';
+}
+?>
