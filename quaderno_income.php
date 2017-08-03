8c5d4c8ea0eb681f53641ad71b640501de03ffd8
Carlos Hernández Medina <carlos@recrea.es>
2017-08-03 11:53:39 +0200

    Some typos

--- quaderno_income.php
+++ quaderno_income.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* Quaderno Invoice
+* Quaderno Income
 *
 * @package   Quaderno PHP
 * @author    Quaderno <hello@quaderno.io>
-* @copyright Copyright (c) 2015, Quaderno
+* @copyright Copyright (c) 2017, Quaderno
 * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
 */
 
